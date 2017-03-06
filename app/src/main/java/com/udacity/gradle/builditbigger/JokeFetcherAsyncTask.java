a7055c9861df042f89d46618d582c94e19ba33a5
Darshan Dorai <darsh.d2@gmail.com>
2017-03-07 05:13:00 +0530

    Update documentation

--- app/src/main/java/com/udacity/gradle/builditbigger/JokeFetcherAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeFetcherAsyncTask.java
@@ -7,6 +7,7 @@ import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest;
 
 import java.io.IOException;
 
@@ -15,6 +16,32 @@ import java.io.IOException;
  */
 
 public class JokeFetcherAsyncTask extends AsyncTask<Void, Void, String> {
+    /**
+     * <p>On adding a new Endpoint and running a Gradle build thereafter,
+     * the following classes are auto-generated:
+     * <ul>
+     *     <li>Api class which additionally has a Builder class and an ApiMethod class.</li>
+     *     <li>ApiRequest</li>
+     *     <li>ApiRequestInitializer</li>
+     *     <li>ApiScope</li>
+     *     <li>The response model class found in the model folder</li>
+     * </ul>
+     *
+     * <p>The above class files can be found here -
+     * "$(PATH_TO_GCE_MODULE)/build/classes/endpointsSrc/$(PACKAGE_NAME)/$(API_NAME)/",
+     * while the source code of these class files are available in -
+     * "$(PATH_TO_GCE_MODULE)/build/generated-source/endpoints/java/$(PACKAGE_NAME)/$(API_NAME)/"
+     *
+     * <p>The Api class has a inner class for each Api method with a unique
+     * rest path. The ApiMethod class is a subclass of ApiRequest class which
+     * in turn is a subclass of {@link com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest
+     * AbstractGoogleJsonClientRequest}. Hence to make a request,
+     * {@link AbstractGoogleJsonClientRequest#execute()} has to be called.
+     *
+     * <p>In this scenario, {@link JokeApi} is the class generated for the api defined
+     * in JokeEndpoint. Calling {@link JokeApi#fetchJoke()} returns the AbstractGoogleClientRequest
+     * object which then is executed to receive the joke.
+     */
     private JokeApi jokeApi;
 
     private JokeFetchedListener jokeFetchedListener;
