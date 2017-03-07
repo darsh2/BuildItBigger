a43cab9847b05f5696860c974abe9859569390e0
Darshan Dorai <darsh.d2@gmail.com>
2017-03-07 06:32:53 +0530

    Refactor code
    Introduced a GCE_ROOT_URL buildConfigField in app/build.gradle so the
    root url for the app engine module can be changed in a single place
    and it will be applied to all buildTypes.
    
    Added documentation regarding JokeApi class and runAndroidTests gradle
    task.

--- app/src/main/java/com/udacity/gradle/builditbigger/JokeFetcherAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeFetcherAsyncTask.java
@@ -59,7 +59,7 @@ public class JokeFetcherAsyncTask extends AsyncTask<Void, Void, String> {
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl(BuildConfig.GCE_ROOT_URL)
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
